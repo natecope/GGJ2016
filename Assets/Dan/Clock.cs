987c1324a5c4774614fdcd1167b58646af10e56a
Kara Thrash <dvdonato@gmail.com>
2016-01-30 18:59:50 -0500

    MainMenuAlarmCLock

---
+++ Assets/Dan/Clock.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+    public Transform[] patrolPoints;
+    public float moveSpeed;
+    private int currentPoint;
+    public bool clockOn;
+    // Use this for initialization
+    void Start () {
+        //AudioSource audio = GetComponent<AudioSource>();
+        clockOn = true;
+        transform.position = patrolPoints[0].position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (clockOn == true)
+        {
+            if (transform.position == patrolPoints[currentPoint].position)
+            {
+                currentPoint++;
+            }
+            if (currentPoint >= patrolPoints.Length)
+            {
+                currentPoint = 0;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position, moveSpeed * Time.deltaTime);
+        }
+    }
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            GetComponent<AudioSource>().Stop();
+            clockOn = false;
+            StartCoroutine(StartTheGame(2.0f));
+            
+        }
+    }
+    
+
+    IEnumerator StartTheGame(float waitTime)
+    {
+        
+        yield return new WaitForSeconds(waitTime);
+        Application.LoadLevel("Dan");
+    }
+}
