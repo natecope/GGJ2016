40609c0c472ed11833d814e1e70a63b7681985c1
Kara Thrash <dvdonato@gmail.com>
2016-01-30 16:49:48 -0500

    ButterSpread

--- Assets/Dan/ButterKnife.cs
+++ Assets/Dan/ButterKnife.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class ButterKnife : MonoBehaviour {
-
+    public bool buttered;
+    public GameObject butterSpot;
+    public GameObject patofButter;
+    
 	// Use this for initialization
 	void Start () {
-	
+        //buttered = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "StickofButter")
+        {
+            //buttered = true;
+           GameObject clone = Instantiate(patofButter, butterSpot.transform.position, butterSpot.transform.rotation) as GameObject;
+            clone.transform.parent = transform;
+        }
+    }
 }
