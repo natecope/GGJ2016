22702405cc19ed9aa404b890439e2cf4e2498764
Kara Thrash <dvdonato@gmail.com>
2016-01-30 09:13:01 -0500

    CoffeeMachine

---
+++ Assets/Dan/CoffeeMachine.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoffeeMachine : MonoBehaviour {
+    public bool makeCoffee;
+    public bool canMakeCoffee;
+    public GameObject coffee;
+    public GameObject gun;
+	// Use this for initialization
+	void Start () {
+        makeCoffee = false;
+        canMakeCoffee = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Kcup")
+        { canMakeCoffee = true;
+            Destroy(col.gameObject);
+        }
+        print("hit");
+        if (col.gameObject.name != "Kcup")
+        {
+            if (canMakeCoffee == true)
+            {
+                if (makeCoffee == true)
+                {
+
+                    makeCoffee = false;
+                }
+                if (makeCoffee == false)
+                {
+                    makeCoffee = true;
+                    StartCoroutine(TurnOn(0.05F));
+
+                }
+            }
+        }
+        
+    }
+    IEnumerator TurnOn(float waitTime)
+    {
+        if (makeCoffee == false)
+        {
+            //StopCoroutine("TurnOn");
+            yield break;
+        }
+        Instantiate(coffee, gun.transform.position, coffee.transform.rotation);
+        yield return new WaitForSeconds(waitTime);
+        
+        StartCoroutine(TurnOn(0.05F));
+    }
+}
---
+++ Assets/Dan/Object.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object : MonoBehaviour {
+    public bool grabbed;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (grabbed == true)
+        { transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); }
+	}
+    public void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(
+     new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        //grabbed = true; 
+    }
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+        if (Input.GetKey(KeyCode.D))
+        {
+            screenPoint.z += 0.1f;
+            //transform.Translate(Vector3.forward * Time.deltaTime); 
+        }
+        if (Input.GetKey(KeyCode.A))
+        { //transform.Translate(Vector3.forward * -Time.deltaTime);
+            screenPoint.z -= 0.1f;
+        }
+    }
+}
