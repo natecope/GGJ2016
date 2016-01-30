27a63089b6c2aeef8e79fdb0e3b2e01f4e2667ba
Kyle Maguire <kylepixel@gmail.com>
2016-01-30 18:40:33 -0500

    Added new arms, separated.

---
+++ Assets/Kyle/ArmsTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmsTest : MonoBehaviour {
+
+	const float ADJUST_FACTOR = 0.1f;
+
+	public Rigidbody wristRigidbody;
+	public Transform armTarget;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		Vector3 difference = armTarget.position - transform.position;
+		wristRigidbody.AddForce(difference * ADJUST_FACTOR, ForceMode.VelocityChange);
+	}
+}
