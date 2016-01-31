affb9e28ec64cabf96d7b5522a50761b9e1ef5b8
Chrs2021 <chrs2021@gmail.com>
2016-01-30 19:22:43 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Models.meta

---
+++ Assets/Topher/WristAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Topher
+{
+    public class WristAction : MonoBehaviour
+    {
+       void OnTriggerStay(Collider other)
+        {
+            messageData data = new messageData();
+        Debug.Log("Collide! " + other.gameObject.tag);
+
+            this.gameObject.SendMessageUpwards("isGrabButtonPressed", data);
+            if(data.isButtonPressed)
+            {
+                Debug.Log("We're grabbing!!");
+                if (other.attachedRigidbody)
+                    other.attachedRigidbody.transform.SetParent(this.transform);
+
+            }
+        }
+    }
+}
