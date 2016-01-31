f72de41be735328c2f3f8da6b45a5ecb541d1d60
Nathan Cope <natecope@gmail.com>
2016-01-31 10:25:10 -0500

    Head controller started

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 using InControl;
 using Assets.Topher;
 
-public enum PlayerType {LeftArm, RightArm}
+public enum PlayerType {LeftArm, RightArm, Head}
 
 public class Player : MonoBehaviour
 {
 	public ArmActions Actions { get; set; }
+	public HeadActions HeadActions { get; set; }
 	public PlayerType playerType;
 	public float wristExtendSpeed;
 	public float wristRotateSpeed;
@@ -23,6 +24,7 @@ public class Player : MonoBehaviour
     //making moveable shoulder
     public GameObject shoulderObj;
     public GameObject mount;
+	public GameObject mainCam;
 	void OnDisable()
 	{
 		if (Actions != null)
@@ -38,21 +40,21 @@ public class Player : MonoBehaviour
         if (playerType == PlayerType.LeftArm)
         {
             mount = GameObject.Find("LeftHand Mount");
-            shoulderObj.GetComponent<HingeJoint>().connectedBody = mount.GetComponent<Rigidbody>();
+            //shoulderObj.GetComponent<HingeJoint>().connectedBody = mount.GetComponent<Rigidbody>();
         }
-        }
+		if (playerType == PlayerType.RightArm){
+			mount = GameObject.Find("RightHand Mount");
+			//shoulderObj.GetComponent<HingeJoint>().connectedBody = mount.GetComponent<Rigidbody>();
+
+		}
+    }
 
     
     void FixedUpdate()
     {
-        if (Actions == null)
-        {
-            // If no controller exists for this cube, just make it translucent.
-            //cachedRenderer.material.color = new Color( 1.0f, 1.0f, 1.0f, 0.2f );
-        }
-        else
-        {
-			
+		if(Actions!=null)
+		{
+
 			Rigidbody wristRigidbody = (Rigidbody)wrist.GetComponent("Rigidbody");
 			Rigidbody elbowRigidbody = (Rigidbody)elbow.GetComponent("Rigidbody");
 			
@@ -70,7 +72,7 @@ public class Player : MonoBehaviour
 
 				elbowRigidbody.AddRelativeTorque((Vector3.down * Actions.ElbowExtendAxis * elbowExtendSpeed), ForceMode.VelocityChange);
 
-			}
+			} 
 
             if (Actions.Grab.IsPressed)
             {
@@ -82,6 +84,20 @@ public class Player : MonoBehaviour
             }
 
         }
+
+		if(HeadActions != null){
+			if(playerType == PlayerType.Head){
+
+				if(HeadActions.Grab.IsPressed){
+
+					Debug.Log("Head action button pressed!");
+
+				}
+
+			}
+
+
+		}
 
     }
     void isGrabButtonPressed(messageData data)
